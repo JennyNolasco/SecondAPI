163695a5467f669cc7bd25a88f3dc652ee788c8e
Jeniffer <jenifferferreira@brq.com>
2021-04-07 20:21:09 -0300

    API 2 - Aula 3 - Parte 1

--- api/routes/agendamentos/index.js
+++ api/routes/agendamentos/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
+const TabelaAgendamento = require('../../agendamentos/TabelaAgendamento');
 
-router.use('/agendamentos', (req, resp) => {
-    resp.send('OK')
+router.get('/agendamentos', async (req, resp) => {
+    const results = await TabelaAgendamento.listar()
+    resp.send(JSON.stringify(results));
+});
+
+router.post('/agendamentos', async (req, resp) => {
+    const reqAgendamento = req.body;
+    
 });
 
 module.exports = router
