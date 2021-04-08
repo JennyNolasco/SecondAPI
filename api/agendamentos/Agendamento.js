a05e678a62a8fe5cbe20d0b7a2ad70a891efd8f6
Jeniffer <jenifferferreira@brq.com>
2021-04-07 22:04:34 -0300

    API 2 - Aula 3 - Parte final

--- api/agendamentos/Agendamento.js
+++ api/agendamentos/Agendamento.js
@@ -1,3 +1,5 @@
+const TabelaAgendamento = require('./TabelaAgendamento')
+
 class Agendamento {
     constructor({id, nome_cliente, nome_servico, status, data_agendamento,
     data_criacao, data_atualizacao}) {
@@ -9,4 +11,31 @@ class Agendamento {
         this.data_criação = data_criacao;
         this.data_atualizacao = data_atualizacao;
     }
+
+    async criar(){
+        const result = await TabelaAgendamento.adicionar({
+            nome_cliente:  this.nome_cliente,
+            nome_servico: this.nome_servico,
+            status: this.status,
+            data_agendamento: this.data_agendamento
+        });
+        this.id = result.id;
+        this.data_criação = result.data_criacao;
+        this.data_atualizacao = result.data_atualizacao;
+    };
+
+    async buscar() {
+        const result = await TabelaAgendamento.buscarPorPK(this.id);
+        this.nome_servico = result.nome_servico;
+        this.nome_cliente = result.nome_cliente;
+        this.status = result.status;
+        this.data_agendamento = result.data_agendamento;
+        this.data_criação = result.data_criacao;
+        this.data_atualizacao = result.data_atualizacao;
+    }
+
+    async remover() {
+        await TabelaAgendamento.remover(this.id)
+    }
 }
+module.exports = Agendamento;
--- api/agendamentos/TabelaAgendamento.js
+++ api/agendamentos/TabelaAgendamento.js
@@ -1,3 +1,4 @@
+const { removeAllListeners } = require('nodemon');
 const modeloAgendamento = require('./modelTabelaAgendamento');
 
 module.exports = {
@@ -9,5 +10,17 @@ module.exports = {
 
     async adicionar(agendamento) {
         return await modeloAgendamento.create(agendamento);
+    },
+
+    async buscarPorPK(id) {
+        return await modeloAgendamento.findByPk(id);
+    },
+
+    async remover(id) {
+        return await modeloAgendamento.destroy({
+            where: {
+                id: id
+            }
+        });
     }
 }
