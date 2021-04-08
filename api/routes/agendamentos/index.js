288b635d1c7f445a330f255fba02298e9e02a5a5
Jeniffer <jenifferferreira@brq.com>
2021-04-07 22:04:43 -0300

    API 2 - Aula 3 - Parte final

--- api/routes/agendamentos/index.js
+++ api/routes/agendamentos/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const TabelaAgendamento = require('../../agendamentos/TabelaAgendamento');
+const Agendamento = require('../../agendamentos/Agendamento')
 
 router.get('/agendamentos', async (req, resp) => {
     const results = await TabelaAgendamento.listar()
@@ -8,7 +9,38 @@ router.get('/agendamentos', async (req, resp) => {
 
 router.post('/agendamentos', async (req, resp) => {
     const reqAgendamento = req.body;
-    
+    const agendamento = new Agendamento(reqAgendamento);
+    await agendamento.criar()
+    resp.send(JSON.stringify(agendamento))
+});
+
+router.get('/agendamentos/:idAgendamento', async (req, resp) => {
+    try {
+        const id = req.params.idAgendamento;
+        const agendamento = new Agendamento({id:id});
+        await agendamento.buscar();
+        resp.send(JSON.stringify(agendamento));
+    } catch (error) {
+        resp.send(JSON.stringify({
+            mensagem: error.message   
+        }));
+    }
+});
+
+router.delete('/agendamentos/:idAgendamento', async (req, resp) => {
+    try {
+        const id = req.params.idAgendamento;
+        const agendamento = new Agendamento({id:id});
+        await agendamento.remover();
+        resp.send(JSON.stringify({
+            mensagem:'Registro removido'
+            })
+        );
+    } catch (error) {
+        resp.send(JSON.stringify({
+            mensagem: error.message
+        }))
+    }
 });
 
 module.exports = router
